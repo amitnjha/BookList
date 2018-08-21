0c72fa1839c1b6a3449a6f120974aa3612003c3d
Amit Jha <amitnjha@gmail.com>
2018-08-20 22:41:59 -0500

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript the good parts' },
+    { title: 'Harry Potter' },
+    { title: 'Lord of the rings' },
+    { title: 'That used to be us' }
+  ];
+}
